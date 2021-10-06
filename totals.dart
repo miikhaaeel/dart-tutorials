af4c506e05eb98336c3e43bd1cd9f85843d380cd
Miikhaaeel <fadowomichael1@gmail.com>
2021-10-06 13:26:45 +0100

    data processing

--- totals.dart
+++ totals.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+
 void main(List<String> args) {
-  print(args);
+  if (args.isEmpty) {
+    print("usage: dart totals.dart <inputFile.csv>");
+    exit(1);
+  }
+
+  final inputFile = args.first;
+  final lines = File(inputFile).readAsLinesSync();
+  for (var line in lines) {
+    print(line);
+  }
 }
