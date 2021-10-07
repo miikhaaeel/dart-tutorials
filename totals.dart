be4e89977e41ee8422443828ae1cdbbac6520df4
Miikhaaeel <fadowomichael1@gmail.com>
2021-10-07 14:06:36 +0100

    data processing

--- totals.dart
+++ totals.dart
@@ -8,7 +8,28 @@ void main(List<String> args) {
 
   final inputFile = args.first;
   final lines = File(inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double>{};
+  lines.removeAt(0);
+  var totalDuration = 0.0;
   for (var line in lines) {
-    print(line);
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null) {
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] == previousTotal + duration;
+    }
+    totalDuration += duration;
   }
+
+  for (var entry in totalDurationByTag.entries) {
+    final tag = entry.key == '' ? 'Unallocated' : entry.key;
+    final durationFormatted = entry.value.toStringAsFixed(1);
+    print('$tag: ${durationFormatted}h');
+  }
+
+  print('Total duration: ${totalDuration.toStringAsFixed(1)}');
 }
