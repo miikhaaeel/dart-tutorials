d5d399c0bb6bb0cac0819b8df3866a30985cc2f6
Miikhaaeel <fadowomichael1@gmail.com>
2021-10-08 11:53:23 +0100

    functions

---
+++ functions.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  const name = 'Michael';
+  const age = 54;
+  describe(name, age);
+
+  sum([45,90,34,23]);
+}
+
+void describe(String name, int age) {
+  print('My name is $name and I\'m $age years old');
+}
+
+double sum(List<double> numbers) {
+  double sum = 0.0;
+  for (var number in numbers) {
+    sum += number;
+  }
+  print(sum);
+  return sum;
+}
