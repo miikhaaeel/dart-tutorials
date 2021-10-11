aacad9091088f6ef73aca835ff081dc8b3567094
Miikhaaeel <fadowomichael1@gmail.com>
2021-10-11 13:54:43 +0100

    typedef

--- anonymous_function.dart
+++ anonymous_function.dart
@@ -1,10 +1,27 @@
+typedef Greet = String Function(String);
 void main() {
   final sayHi = (String name) => 'Hi, $name';
 
-  welcome(sayHi,'Michael');
+  welcome(sayHi, 'Michael');
+
+  print(greetEN('Miko'));
+  print(greetFR('Miko'));
+  print(greetESP('Miko'));
 }
 
-void welcome(String Function(String) greet, String name) {
+void welcome(Greet greet, String name) {
   print(greet(name));
   print('Welcome to the club');
 }
+
+Greet greetEN = (name) {
+  return 'Hello $name';
+};
+
+Greet greetFR = (name) {
+  return 'Bonjour $name';
+};
+
+Greet greetESP = (name) {
+  return 'Hola $name';
+};
