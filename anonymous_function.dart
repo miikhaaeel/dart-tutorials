59f631ec285305c581d215b5f1ce2392ce2ce600
Miikhaaeel <fadowomichael1@gmail.com>
2021-10-12 23:59:57 +0100

    forEach

--- anonymous_function.dart
+++ anonymous_function.dart
@@ -7,6 +7,17 @@ void main() {
   print(greetEN('Miko'));
   print(greetFR('Miko'));
   print(greetESP('Miko'));
+
+  const multiplier = 10;
+  const list = [1, 2, 3, 4, 5];
+  final result = list.map((x) => x * multiplier); //closure
+  print(result);
+
+  list.forEach((element) {
+    print(element);
+  });
+
+  list.forEach(print); //why? Dart will implicitly pass in each values in list to print
 }
 
 void welcome(Greet greet, String name) {
